ee858d94ec585e03dac8153b084dd66b6e61407f
z-ach <zachkaufman97@gmail.com>
2017-01-14 16:39:58 -0800

    fixed header in Orc.java

--- src/edu/cpp/cs/cs141/danddgame/being/enemies/Orc.java
+++ src/edu/cpp/cs/cs141/danddgame/being/enemies/Orc.java
@@ -1,5 +1,12 @@
 /**
- * 
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #1
+ *
+ * create and design architecture for dnd video game
+ *
+ * Zachary Kaufman
  */
 package edu.cpp.cs.cs141.danddgame.being.enemies;
 
