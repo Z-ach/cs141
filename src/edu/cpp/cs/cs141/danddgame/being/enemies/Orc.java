51ccf8226c2f7728f6152431f388dbc20fefa152
z-ach <zachkaufman97@gmail.com>
2017-01-17 14:54:43 -0800

    added better documentation for Orc.java

--- src/edu/cpp/cs/cs141/danddgame/being/enemies/Orc.java
+++ src/edu/cpp/cs/cs141/danddgame/being/enemies/Orc.java
@@ -10,6 +10,8 @@
  */
 package edu.cpp.cs.cs141.danddgame.being.enemies;
 
+import edu.cpp.cs.cs141.danddgame.being.heros.GeneralCharacter;
+
 /**
  * @author Zach Kaufman
  *
@@ -17,24 +19,35 @@ package edu.cpp.cs.cs141.danddgame.being.enemies;
 public class Orc extends GeneralEnemy {
 
 	/**
-	 * How much experience a player gets from killing this enemy.
+	 * This field represents how much experience the {@link GeneralEnemy} contains. This value will be given to a
+	 * {@link edu.cpp.cs.cs141.danddgame.being.heros.GeneralCharacter} when this {@link GeneralEnemy} is killed.
+	 * 
+	 * @see #getExperience()
 	 */
 	private final static int EXPERIENCE = 5;
 	
 	/**
-	 * Witch constructor. Makes a call to GeneralEnemy which calls Being, and assigns it a name.
+	 * This is the constructor for the {@link Orc} enemy. {@link Orc} is a subclass of {@link GeneralEnemy}, so it
+	 * inherits everything within it. The {@link Orc} enemy is designed to have more defense than other enemies,
+	 * so this constructor adds additional defense.
 	 * 
+	 * @see edu.cpp.cs.cs141.danddgame.being.enemies.GeneralEnemy#GeneralEnemy(String, int)
 	 */
 	public Orc() {
 		super("Orc", EXPERIENCE);
 		addDefense(4);
 	}
 	
-	/**
-	 * Smashes the character, deals a set amount of damage
-	 */
-	public void smash() {
 
+	/**
+	 * This method is the main attack for the {@link Orc}. Every time this method is called, it will deal some sort of
+	 * damage to its target.
+	 * 
+	 * @param target the recipient of the {@link Orc}'s attack.
+	 * @return the amount of damage to deal to the target
+	 */
+	public int smash(GeneralCharacter target) {
+		return 0;
 	}
 
 }
