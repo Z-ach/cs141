c008a1909647ba14e9f8de271d3fdd9cc76e91c5
z-ach <zachkaufman97@gmail.com>
2017-01-17 18:25:26 -0800

    added main again, needed to compile

---
+++ src/edu/cpp/cs/cs141/danddgame/Game.java
@@ -0,0 +1,25 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Programming Assignment #1
+ *
+ * create and design architecture for dnd video game
+ *
+ * Zachary Kaufman
+ */
+package edu.cpp.cs.cs141.danddgame;
+
+public class Game {
+
+
+	/**
+	 * The main method for the Homework1 assignment. {@link #main(String[])} is required for a program to
+	 * compile, and thus is required here, as the program's compilation ability is one of the submission
+	 * requirements.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
